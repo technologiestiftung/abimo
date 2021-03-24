3baab869826b8e7c6f877ea6a2b03dcb2d71a07b
Andreas Matzinger <andreas.matzinger@kompetenz-wasser.de>
2021-03-24 11:56:14 +0100

    ADD Abimo source code

---
+++ src/dbaseField.cpp
@@ -0,0 +1,79 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Meiko Rachimow, Claus Rachimow                  *
+ *   This file is part of Abimo 3.2                                        *
+ *   Abimo 3.2 is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "dbaseField.h"
+
+DbaseField::DbaseField()
+{
+    fieldLength = 0;
+    decimalCount = 0;
+}
+
+DbaseField::DbaseField(QByteArray array)
+{
+    this->name = QString(array.left(10));
+    this->type = QString((QChar)array[11]);
+    this->fieldLength = (quint8)array[16];
+    this->decimalCount = (quint8)array[17];
+}
+
+DbaseField::DbaseField(QString name, QString type, int decimalCount)
+{
+    this->name = QString(name);
+    this->type = QString(type);
+    this->fieldLength = 0;
+    this->decimalCount = decimalCount;
+}
+
+void DbaseField::set(QString name, QString type, int decimalCount)
+{
+    this->name = QString(name);
+    this->type = QString(type);
+    this->fieldLength = 0;
+    this->decimalCount = decimalCount;
+}
+
+DbaseField::~DbaseField()
+{
+}
+
+QString DbaseField::getName()
+{
+    return name;
+}
+
+QString DbaseField::getType()
+{
+    return type;
+}
+
+int DbaseField::getFieldLength()
+{
+    return fieldLength;
+}
+
+void DbaseField::setFieldLength(int fl)
+{
+    fieldLength = fl;
+}
+
+int DbaseField::getDecimalCount()
+{
+    return decimalCount;
+}
