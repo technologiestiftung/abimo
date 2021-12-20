95df12d2f9f129332fb3aae4a6b7c8839df29bcd
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-12-20 11:00:13 +0100

    Set version to 3.2.2, use only 3.2 in program name

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define VERSION_STRING "3.2.1 (KWB, 2021-12-20)"
-#define PROGRAM_NAME "Abimo 3.2.1"
+#define VERSION_STRING "3.2.2 (KWB, 2021-12-20)"
+#define PROGRAM_NAME "Abimo 3.2"
 
 #endif // CONSTANTS_H
