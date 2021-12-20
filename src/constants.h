1f371bfd8cb0a3b2d66ee7ac956b2c9c61a5c858
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-12-20 11:17:16 +0100

    back to development version

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define VERSION_STRING "3.2.2 (KWB, 2021-12-20)"
+#define VERSION_STRING "3.2.2.9999 (KWB, under development)"
 #define PROGRAM_NAME "Abimo 3.2"
 
 #endif // CONSTANTS_H
