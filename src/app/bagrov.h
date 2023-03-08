5839060bfabc432bf2d9237c9c9dc02ac9c54d0e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-03-08 01:44:12 +0100

    Cosmetics in bagrov.h

--- src/app/bagrov.h
+++ src/app/bagrov.h
@@ -3,16 +3,14 @@
  * of this repository (https://github.com/KWB-R/abimo).
  ***************************************************************************/
 
-#ifndef BAGROV_H /* Prevent multiple includes */
+#ifndef BAGROV_H
 #define BAGROV_H
 
 class Bagrov
 {
-
 public:
     static float nbagro(float bage, float x);
     static void bagrov(float *bagf, float *x0, float *y0);
-
 private:
     const static float aa[];
 };
