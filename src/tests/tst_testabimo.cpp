43452084accd72e365c737c1ee4de9e46f1440bf
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-03-20 17:58:09 +0100

    Compare file sizes

--- src/tests/tst_testabimo.cpp
+++ src/tests/tst_testabimo.cpp
@@ -188,25 +188,27 @@ void TestAbimo::test_config_getTWS()
 
 void TestAbimo::test_calc()
 {
-    QString inputFile = dataFilePath("abimo_2019_mitstrassen.dbf");
-    QString configFile = dataFilePath("config.xml");
-    QString tmpOut_noConfig = dataFilePath("tmp_out_no-config.dbf", false);
-    QString tmpOut_config = dataFilePath("tmp_out_config.dbf", false);
-    QString refOut_noConfig = dataFilePath("abimo_2019_mitstrassenout_3.2.1_default-config.dbf");
-    QString refOut_config = dataFilePath("abimo_2019_mitstrassenout_3.2.1_xml-config.dbf");
+    QFile inputFile(dataFilePath("abimo_2019_mitstrassen.dbf"));
+    QFile configFile(dataFilePath("config.xml"));
+    QFile tmpOut_noConfig(dataFilePath("tmp_out_no-config.dbf", false));
+    QFile tmpOut_config(dataFilePath("tmp_out_config.dbf", false));
+    QFile refOut_noConfig(dataFilePath("abimo_2019_mitstrassenout_3.2.1_default-config.dbf"));
+    QFile refOut_config(dataFilePath("abimo_2019_mitstrassenout_3.2.1_xml-config.dbf"));
 
     // Run the simulation without config file
-    Calculation::runCalculation(inputFile, "", tmpOut_noConfig, false);
-    QVERIFY(dbfHeadersAreIdentical(tmpOut_noConfig, refOut_noConfig));
-    QVERIFY(dbfStringsAreIdentical(tmpOut_noConfig, refOut_noConfig));
+    Calculation::runCalculation(inputFile.fileName(), "", tmpOut_noConfig.fileName(), false);
+    QVERIFY(tmpOut_noConfig.size() == refOut_noConfig.size());
+    QVERIFY(dbfHeadersAreIdentical(tmpOut_noConfig.fileName(), refOut_noConfig.fileName()));
+    QVERIFY(dbfStringsAreIdentical(tmpOut_noConfig.fileName(), refOut_noConfig.fileName()));
 
     // Files are not identical due to differing dates in the header
     //QVERIFY(helpers::filesAreIdentical(outputFile, referenceFile));
 
     // Run the simulation with initial values from config file
-    Calculation::runCalculation(inputFile, configFile, tmpOut_config);
-    QVERIFY(dbfHeadersAreIdentical(tmpOut_config, refOut_config));
-    QVERIFY(dbfStringsAreIdentical(tmpOut_config, refOut_config));
+    Calculation::runCalculation(inputFile.fileName(), configFile.fileName(), tmpOut_config.fileName());
+    QVERIFY(tmpOut_config.size() == refOut_config.size());
+    QVERIFY(dbfHeadersAreIdentical(tmpOut_config.fileName(), refOut_config.fileName()));
+    QVERIFY(dbfStringsAreIdentical(tmpOut_config.fileName(), refOut_config.fileName()));
 
     // file created using the GUI
     //QString file_tmp = dataFilePath("abimo_2019_mitstrassen_out2.dbf", true);
